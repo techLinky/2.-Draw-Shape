886d2d76e3c53ba4ba3b54a4c88240e4e1107276
techLinky <gcebile.sne@gmail.com>
2023-09-11 09:31:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("    /|");
+            Console.WriteLine("    /|");
+            Console.WriteLine("    /|");
+            Console.WriteLine("    /|");
+            Console.WriteLine(" -----");
+            //to freeze the console
+            Console.ReadLine();
+        }
+    }
+}
